962c3570e54b1f881e657b52aba26fc8415cc74c
Jsaini1727 <jsaini1727@gmail.com>
2024-02-28 17:43:57 -0500

    Finished the Readme

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,8 @@
 export {default as sunny} from './projects/sunny.png'
+
+export {default as instalLasagna} from './projects/instalLasagna.png'
+
+export {default as construction} from './projects/underConstruction.png'
+
+
+
